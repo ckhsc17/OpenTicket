f0c788474c0c33c6e3d9baed5f332e7a2a62c5fa
Ocean1029 <vanessa0201029@gmail.com>
2024-12-08 18:00:06 +0800

    更新 order crud 的邏輯，exception handling 讓系統遇到不存在的活動或用戶時不會 internal error

--- app/routers/orders.py
+++ app/routers/orders.py
@@ -1,37 +1,44 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.schemas import EventCreate, EventOut, UserOut, OrderCreate, OrderOut
-from app.models import Event, Seat, Ticket, Order
-from app.dependencies import user_dependency, db_dependency
-#from app.routers.auth import SECRET_KEY, ALGORITHM  # 导入 SECRET_KEY 和 ALGORITHM
-from app.crud import get_user, get_user_by_email, create_order, get_orders_by_event, update_order
-from app.crud import get_event, get_events, create_event, join_event, leave_event
+from app.models import User, Event, Order, OrderStatus
+from app.crud import  create_order,  update_order
 from sqlalchemy.orm import Session
 from app.database_connection import get_db
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from typing import Optional
-import os
-from dotenv import load_dotenv
-from pydantic import BaseModel
 from typing import List
-from fastapi.security import OAuth2PasswordBearer
 
 router = APIRouter()
 
 @router.get("/orders/{user_id}", response_model=List[OrderOut], tags=["Orders"])
 def get_orders_by_user(user_id: int, db: Session = Depends(get_db)):
+    
+    # check if user is in the database
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
     order = db.query(Order).filter(Order.user_id == user_id).all()
+
     return order
 
 @router.get("/orders/{event_id}", response_model=List[OrderOut], tags=["Orders"])
 def get_orders_by_event(event_id: int, db: Session = Depends(get_db)):
+
+    # check if event is in the database
+    event = db.query(Event).filter(Event.event_id == event_id).first()
+    if not event:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event not found")
+
     order = db.query(Order).filter(Order.event_id == event_id).all()
     return order
 
 @router.post("/orders", response_model=OrderOut, tags=["Orders"])
 def create_order_for_user(order: OrderCreate, db: Session = Depends(get_db)):
-    print("hi from create_order_for_user")
+
+    # check if user is in the database
+    user = db.query(User).filter(User.user_id == order.user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
     print("order", order)
     new_order = create_order(db, order)
     return new_order
@@ -39,5 +46,9 @@ def create_order_for_user(order: OrderCreate, db: Session = Depends(get_db)):
 # cancel an order
 @router.put("/orders/{order_id}/cancel", tags=["Orders"])
 async def cancel_order(order_id: int, db: Session = Depends(get_db)):
-    print("hi from cancel_order")
-    update_order(db, order_id, "canceled")
+    
+    order = db.query(Order).filter(Order.order_id == order_id).first()
+    if not order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+    
+    update_order(db, order_id, OrderStatus.Canceled)
