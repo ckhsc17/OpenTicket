35e962f71e694b1bc9d8b0fe46b1147cb9df825a
Ocean1029 <vanessa0201029@gmail.com>
2024-12-09 22:38:52 +0800

    Enhance seat management functionality: Added event retrieval and improved seat confirmation logic. Refactored confirm_seat function to streamline ticket creation and removed unused user authentication checks. Updated error handling for event and seat availability.

--- app/routers/seats.py
+++ app/routers/seats.py
@@ -1,3 +1,4 @@
+from arrow import get
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
@@ -6,7 +7,7 @@ from typing import List
 from app.dependencies import get_current_user
 from app.schemas import TicketCreate, OrderCreate
 from app.models import Event
-from app.crud import create_tickets, create_order, update_seat, get_seats
+from app.crud import create_tickets, create_order, update_seat, get_seats, get_event
 from app.routers.tickets import get_designated_seats
 from app.database_connection import get_db
 
@@ -20,42 +21,44 @@ router = APIRouter()
 # Search for available seats by event
 @router.get("/events/{event_id}/get_seats", tags=["Seats"])
 def get_all_seats(event_id: int, db: Session = Depends(get_db)):
-    event = db.query(Event).filter(Event.event_id == event_id).first()
-    print("hi from get_seats")
+    event = get_event(db, event_id)
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+    
     venue_id = event.venue_id
-    print("venue_id", venue_id)
     #reserved_seats = db.query(Ticket).filter(Ticket.event_id == event_id).all() #如果ticket是動態生成的，這裡會有問題
+    
     seats = get_seats(db, venue_id)
     
     return seats
 
 # Confirm a seat, check if it's available then create an order and ticket
 @router.post("/events/{event_id}/confirm_seat", tags=["Seats"])
-def confirm_seat(ticket: TicketCreate, order: OrderCreate, current_user = Depends(get_current_user), db: Session = Depends(get_db)): #event_id到時會由前端傳入
-    event = db.query(Event).filter(Event.event_id == ticket.event_id).first()
+def confirm_seat(
+    ticketCreate: TicketCreate, 
+    orderCreate: OrderCreate, 
+    db: Session = Depends(get_db)
+    ):
     #ticket.venue_id = event.venue_id #venue_id由後端取得
     #reserved_seats = db.query(Ticket).filter(Ticket.event_id == event_id).all()
-    if (get_designated_seats(ticket.event_id, ticket.seat_number, db) == None):
+    
+    if (get_designated_seats(ticketCreate.event_id, ticketCreate.seat_number, db) == None):
         raise HTTPException(status_code=404, detail="Seat not found")
     #檢查designated_seats是否有人:待實作
 
     # Check if the user is logged in
-    if not current_user:
-        raise HTTPException(status_code=401, detail="Unauthorized")
     '''
     reserved_seat_numbers = [ticket.seat_number for ticket in reserved_seats]
     
     if seat_number in reserved_seat_numbers:
         raise HTTPException(status_code=400, detail="Seat is already taken")
     '''
-    new_order = create_order(db, order, current_user.get('id'))
-    #get order_id
-    #order_id = db.query(Order).filter(Order.user_id == current_user.user_id).first().order_id
-    ticket = create_tickets(db, ticket)  
+    new_order = create_order(db, orderCreate)
+    
+    # create multiple tickets
+    ticketCreate = create_tickets(db, ticketCreate)  
     #order.user_id = current_user.user_id
-    
-    
-    return {"order_id": new_order.order_id, "ticket_id": ticket.ticket_id}
+    return {"order_id": new_order.order_id, "ticket_id": ticketCreate.ticket_id}
 
 '''
 # update seat status
