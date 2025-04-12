30f0b4df44a6ddd42879e58428310ba22afdbe52
ckhsc17 <94750792+Bowenbowenbowenbowen@users.noreply.github.com>
2025-04-12 18:31:06 +0800

    modify_cloudbuild5

--- app/database_connection.py
+++ app/database_connection.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 load_dotenv() # 載入 .env 檔案
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("SUPABASE_DB_URL")
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL is not set")
 
